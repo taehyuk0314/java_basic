37ebd5e6cd9e0157339a74decf89b617771c009a
taehyek0314 <taehyek0314@gamil.com>
2018-12-10 21:00:27 +0900

    12-10-21-00

--- day3/Gender.java
+++ day3/Gender.java
@@ -1,10 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Gender {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("주민등록번호를 입력하세요");
+		Scanner scanner = new Scanner(System.in);
+		String snn = scanner.next();
+		char ch = snn.charAt(7);
+		String result = "";
+		switch(ch) {
+		case'1' : case'3' : result = "남자"; 
+		case'2' : case'4' : result = "여자"; 
+		case'5' : case'6' : result = "외국인";
+		default : result= "잘못된입력입니다";
+		}
+		System.out.println(result);
 	}
+	
 
 }
--- day3/LeapYear.java
+++ day3/LeapYear.java
@@ -1,10 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class LeapYear {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("년도를 입력하세요");
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+		String result = "";
+		if((year%4==0&&year%100!=0)||year%400==0) {
+			result = "윤년";
+		}else {
+			result = "평년";
+		}
+		System.out.println(result);
 	}
 
 }
